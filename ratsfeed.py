8e8cd025b7a7516f0e0e52d0a5294a83b54315f9
Nithil Krishna A R <nithilkrishna97@gmail.com>
2026-09-23 15:20:45 +0530

    Update ratsfeed.py

--- ratsfeed.py
+++ ratsfeed.py
@@ -2,18 +2,15 @@ r = int(input("number of rats"))
 unit = int(input("the amount of food each rat consumes"))
 n = int(input("number of house"))
 arr = list(map(int, input().split()))
-
 if arr is None:
     print(-1)
 else:
     required = r * unit
     total = 0
     houses= 0
-
-    for i in range(n):
+  for i in range(n):
         total += arr[i]
         if total >= required:
             houses= i + 1
             break
-
-    print("HOUSES:",houses)
+ print("HOUSES:",houses)
